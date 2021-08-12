dfaa050aaadc8e824ec8b638e62433315dd73a13
Prince <princem.ec.19@nitj.ac.in>
2021-08-12 19:22:37 +0530

    removed errors happened during making home branch

--- chaitanya/views.py
+++
@@ -1,9 +0,0 @@
-from typing import ContextManager
-from django.shortcuts import render
-
-
-def home(request):
-    context = {
-        "title": "Chaitanya - vision to impart knowledge"
-    }
-    return render(request, "home/home.html", context)
