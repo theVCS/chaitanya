bc612bc5acdd05e9de8c4be80425e3050481992f
Prince <princem.ec.19@nitj.ac.in>
2021-10-12 15:45:25 +0530

    made some changes in settings to host

--- chaitanya/settings.py
+++ chaitanya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vmy#@u&a22(y8$mc^llupr(rjb(*wy5%7c^j%gs7prt!tsrasj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["soece.azurewebsites.net", "127.0.0.1"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
